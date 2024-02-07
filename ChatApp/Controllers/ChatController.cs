5893717f40e747c518c3a92fa7380838ed833bc0
KrasimirKolew <krasirumenov789@gmail.com>
2024-02-07 15:09:10 +0200

    Add project files.

---
+++ ChatApp/Controllers/ChatController.cs
@@ -0,0 +1,44 @@
+﻿using ChatApp.Models.Message;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ChatApp.Controllers
+{
+    public class ChatController : Controller
+    {
+        private static List<KeyValuePair<string,string>> messeges = new List<KeyValuePair<string,string>>();
+
+        public IActionResult Show()
+        {
+            if (messeges.Count <1)
+            {
+                return View(new ChatViewModel());
+            }
+
+            var chatModel = new ChatViewModel()
+            {
+                Messages = messeges
+                .Select(m=>new MessageViewModel()
+                {
+                    Sender = m.Key,
+                    MessageText = m.Value
+                })
+                .ToList()
+            };
+
+            return View(chatModel);
+        }
+
+        [HttpPost]
+        public IActionResult Send(ChatViewModel chat) 
+        {
+            var newMess = chat.CurrentMessege;
+
+            messeges.Add(new KeyValuePair<string, string>
+                (newMess.Sender,newMess.MessageText));
+
+            return RedirectToAction("Show");
+        }
+
+
+    }
+}
